4204634574674f9e9e4ea42b90147f019f1ea642
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2021-10-08 09:23:03 +0200

    Summary
    Adding loads of .js files to create a template fro the pages so it easy to change code on al pages at once

---
+++ js/javascript.js
@@ -0,0 +1,44 @@
+var links = [
+    
+    // Home page
+    {
+        name: "Home",
+        url: "/" 
+    },
+
+    // Login/register page
+    {
+        name: "",
+        url: "/"
+    },
+    
+    // Profile page
+    {
+        name: "",
+        url: "/"
+    },
+    
+    // Gameslist page
+    {
+        name: "",
+        url: "/"
+    },
+    
+    // Gameinfo page
+    {
+        name: "",
+        url: "/"
+    },
+
+    // Shoppingchart page
+    {
+        name: "",
+        url: "/"
+    },
+
+    // Contactus page
+    {
+        name: "",
+        url: "/"
+    }
+]
