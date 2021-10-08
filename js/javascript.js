1dd53d6907b69c2b0877b443c0fec62442a52dfa
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2021-10-08 10:34:12 +0200

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -42,3 +42,30 @@ var links = [
         url: "/"
     }
 ]
+
+
+// this is copy paste from Oliver dipple. 
+function createNavItem(item) {
+    var li = document.createElement('li');
+    var a = document.createElement('a');
+    a.href = item.url;
+    a.innerText = item.name;
+    li.append(a);
+    return li;
+  }
+  
+  var ul = document.querySelector('ul.nav');
+  
+  for (var i = 0; i < links.length; i++) {
+    var item = links[i];
+    var itemHtml = createNavItem(item);
+    ul.append(itemHtml);
+  }
+
+
+  function createHeader(header){
+    var header = 
+  document.createElement('header');
+    return header;
+  }
+  
